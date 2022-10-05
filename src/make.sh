12ab682f7b36aefeb42d22318c05222f7a295f22
Gabriel Margarido <lfilipe.ribeiro72@gmail.com>
2022-10-04 22:28:32 -0300

    Fixed build scripts

--- src/make.sh
+++ src/make.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 npm install --save-dev
-npx tsc mason-common.ts
-npx pkg mason-common.js -o mason-common
+npx tsc cmason-common.ts
+npx pkg cmason-common.js -o mason-common
 
-npx tsc mason-dev.ts
-npx pkg mason-dev.js -o mason-dev
+npx tsc cmason-dev.ts
+npx pkg cmason-dev.js -o mason-dev
 
-npx tsc mason-save.ts
-npx pkg mason-save.js -o mason-save
+npx tsc cmason-save.ts
+npx pkg cmason-save.js -o mason-save
