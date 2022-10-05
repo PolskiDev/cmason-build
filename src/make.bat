04325a90664a8cbdf1ad6c528110ab0c17ba4413
Gabriel Margarido <lfilipe.ribeiro72@gmail.com>
2022-10-04 22:36:02 -0300

    Fixed binaries compilation

--- src/make.bat
+++ src/make.bat
@@ -1,10 +1,10 @@
 @echo off
 npm install --save-dev
 npx tsc cmason-common.ts
-npx pkg cmason-common.js -o mason-common
+npx pkg cmason-common.js -o cmason-common
 
 npx tsc cmason-dev.ts
-npx pkg cmason-dev.js -o mason-dev
+npx pkg cmason-dev.js -o cmason-dev
 
 npx tsc cmason-save.ts
-npx pkg cmason-save.js -o mason-save
+npx pkg cmason-save.js -o cmason-save
